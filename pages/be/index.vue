9d0e5ea57c8c7cb062afe835143b04d663c23d9a
pesochec <gcpesochks@gmail.com>
2021-07-02 10:27:51 +0300

    Be and Payments

--- pages/be/index.vue
+++ pages/be/index.vue
@@ -7,13 +7,94 @@
     <h2>
       Booking entries
     </h2>
+    <div>
+      <v-data-table
+        :headers="headers"
+        :items="mock"
+        sort-by="calories"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-toolbar
+            flat
+          >
+            <v-toolbar-title>Booking entries Information</v-toolbar-title>
+            <v-divider
+              class="mx-4"
+              inset
+              vertical
+            ></v-divider>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+        </template>
+        <template v-slot:[`item.actions`]="">
+          <v-icon
+            small
+          >
+            mdi-delete
+          </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn
+            color="primary"
+            @click="initialize"
+          >
+            Refresh
+          </v-btn>
+        </template>
+      </v-data-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  async asyncData ({ $axios }) {
+    const list = await $axios.$get('/api/airports')
+    return {
+      list
+    }
+  },
   data () {
     return {
+      headers: [
+        // {
+        //   text: 'Airport ID',
+        //   align: 'start',
+        //   sortable: false,
+        //   value: 'id'
+        // },
+        { text: 'REF Number', value: 'id' },
+        { text: 'Status', value: 'status' },
+        { text: 'Operator', value: 'operator' },
+        { text: 'Hotel', value: 'hotel' },
+        { text: 'Price', value: 'price' },
+        { text: 'Netto', value: 'netto' },
+        { text: 'Profit', value: 'profit' },
+        { text: 'Departure', value: 'departure' }
+      ],
+      mock: [
+        {
+          id: '0000000000001',
+          status: 'Paid up',
+          operator: 'Dohn Snow',
+          hotel: 'B Ocean Resort',
+          price: '$9,763',
+          netto: '$165,000',
+          profit: '$139,900',
+          departure: '10.10.2019'
+        },
+        {
+          id: '0000000000002',
+          status: 'Not Paid',
+          operator: 'Bessie Cooper',
+          hotel: 'B Ocean Resort',
+          price: '$9,763',
+          netto: '$165,000',
+          profit: '$139,900',
+          departure: '10.10.2019'
+        }
+      ],
       items: [
         {
           text: 'Booking entries',
--- pages/payments/index.vue
+++ pages/payments/index.vue
@@ -7,13 +7,94 @@
     <h2>
       Payments
     </h2>
+    <div>
+      <v-data-table
+        :headers="headers"
+        :items="mock"
+        sort-by="calories"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-toolbar
+            flat
+          >
+            <v-toolbar-title>Incoming </v-toolbar-title>
+            <v-divider
+              class="mx-4"
+              inset
+              vertical
+            ></v-divider>
+            <v-spacer></v-spacer>
+          </v-toolbar>
+        </template>
+        <template v-slot:[`item.actions`]="">
+          <v-icon
+            small
+          >
+            mdi-delete
+          </v-icon>
+        </template>
+        <template v-slot:no-data>
+          <v-btn
+            color="primary"
+            @click="initialize"
+          >
+            Refresh
+          </v-btn>
+        </template>
+      </v-data-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  async asyncData ({ $axios }) {
+    const list = await $axios.$get('/api/airports')
+    return {
+      list
+    }
+  },
   data () {
     return {
+      headers: [
+        // {
+        //   text: 'Airport ID',
+        //   align: 'start',
+        //   sortable: false,
+        //   value: 'id'
+        // },
+        { text: 'REF Number', value: 'id' },
+        { text: 'Res Date', value: 'res' },
+        { text: 'Departure Date', value: 'departure' },
+        { text: 'Trans ID', value: 'trans_id' },
+        { text: 'Payment Date', value: 'payment_date' },
+        { text: 'Amount', value: 'amount' },
+        { text: 'Method', value: 'method' },
+        { text: 'Manager', value: 'manager' }
+      ],
+      mock: [
+        {
+          id: 'BA9212320',
+          res: '07.07.2020',
+          departure: '04.12.2019',
+          trans_id: '7632525',
+          payment_date: '31.10.2019',
+          amount: '$2,638',
+          method: 'Сash',
+          manager: 'Kristin Watson'
+        },
+        {
+          id: 'BA9212320',
+          res: '07.07.2020',
+          departure: '04.12.2019',
+          trans_id: '7632525',
+          payment_date: '31.10.2019',
+          amount: '$2,638',
+          method: 'Non-cash',
+          manager: 'Kristin Watson'
+        }
+      ],
       items: [
         {
           text: 'Payments',
